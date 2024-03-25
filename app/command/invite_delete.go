f77653502b1d82bbf8a8a0cd87314348afb7f7c1
9ssi7 <info@ssibrahimbas.com>
2024-03-26 01:06:56 +0300

    feature/cillop: implement the invite commands

--- app/command/invite_delete.go
+++ app/command/invite_delete.go
@@ -1,1 +1,39 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.business/domains/invite"
+)
+
+type InviteDeleteCmd struct {
+	InviteUUID string `params:"uuid" validate:"required,uuid"`
+	UserUUID   string
+	UserName   string
+}
+
+type InviteDeleteRes struct{}
+
+type InviteDeleteHandler cqrs.HandlerFunc[InviteDeleteCmd, *InviteDeleteRes]
+
+func NewInviteDeleteHandler(repo invite.Repository, events invite.Events) InviteDeleteHandler {
+	return func(ctx context.Context, cmd InviteDeleteCmd) (*InviteDeleteRes, *i18np.Error) {
+		res, err := repo.GetByUUID(ctx, cmd.InviteUUID)
+		if err != nil {
+			return nil, err
+		}
+		_err := repo.Delete(ctx, cmd.InviteUUID)
+		if _err != nil {
+			return nil, _err
+		}
+		events.Delete(invite.InviteDeleteEvent{
+			InviteUUID:       cmd.InviteUUID,
+			UserUUID:         cmd.UserUUID,
+			UserName:         cmd.UserName,
+			BusinessNickName: res.BusinessName,
+		})
+		return &InviteDeleteRes{}, nil
+	}
+}
--- app/command/invite_use.go
+++ app/command/invite_use.go
@@ -1,1 +1,59 @@
 package command
+
+import (
+	"context"
+	"time"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.business/domains/business"
+	"github.com/turistikrota/service.business/domains/invite"
+)
+
+type InviteUseCmd struct {
+	InviteUUID string `params:"uuid" validate:"required,uuid"`
+	UserEmail  string
+	UserUUID   string
+	UserName   string
+}
+
+type InviteUseRes struct{}
+
+type InviteUseHandler cqrs.HandlerFunc[InviteUseCmd, *InviteUseRes]
+
+func NewInviteUseHandler(repo invite.Repository, factory invite.Factory, events invite.Events, businessRepo business.Repository, businessFactory business.Factory) InviteUseHandler {
+	return func(ctx context.Context, cmd InviteUseCmd) (*InviteUseRes, *i18np.Error) {
+		res, err := repo.GetByUUID(ctx, cmd.InviteUUID)
+		if err != nil {
+			return nil, err
+		}
+		if res.Email != cmd.UserEmail {
+			return nil, factory.Errors.EmailMismatch()
+		}
+		if res.IsUsed {
+			return nil, factory.Errors.Used()
+		}
+		if res.IsDeleted {
+			return nil, factory.Errors.Deleted()
+		}
+		if res.CreatedAt.Add(24 * time.Hour).Before(time.Now()) {
+			return nil, factory.Errors.Timeout()
+		}
+		_err := businessRepo.AddUser(ctx, res.BusinessName, businessFactory.NewUser(cmd.UserUUID, cmd.UserName))
+		if _err != nil {
+			return nil, _err
+		}
+		error := repo.Use(ctx, cmd.InviteUUID)
+		if error != nil {
+			return nil, error
+		}
+		events.Use(invite.InviteUseEvent{
+			InviteUUID:       cmd.InviteUUID,
+			UserUUID:         cmd.UserUUID,
+			UserName:         cmd.UserName,
+			UserEmail:        cmd.UserEmail,
+			BusinessNickName: res.BusinessName,
+		})
+		return &InviteUseRes{}, nil
+	}
+}
