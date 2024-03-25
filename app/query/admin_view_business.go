0fe6597a9fb604d1c9f4e6aebc8cd69c9bc9a75c
9ssi7 <info@ssibrahimbas.com>
2024-03-26 01:23:57 +0300

    feature/cillop: add admin view and get with user query

--- app/query/admin_view_business.go
+++ app/query/admin_view_business.go
@@ -1,1 +1,29 @@
 package query
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.business/domains/business"
+)
+
+type AdminViewBusinessQuery struct {
+	NickName string `params:"nickName" validate:"required"`
+}
+
+type AdminViewBusinessRes struct {
+	Business *business.Entity
+}
+
+type AdminViewBusinessHandler cqrs.HandlerFunc[AdminViewBusinessQuery, *AdminViewBusinessRes]
+
+func NewAdminViewBusinessHandler(repo business.Repository) AdminViewBusinessHandler {
+	return func(ctx context.Context, query AdminViewBusinessQuery) (*AdminViewBusinessRes, *i18np.Error) {
+		business, err := repo.GetByNickName(ctx, query.NickName)
+		if err != nil {
+			return nil, err
+		}
+		return &AdminViewBusinessRes{Business: business}, nil
+	}
+}
--- app/query/business_get_with_user.go
+++ app/query/business_get_with_user.go
@@ -1,1 +1,34 @@
 package query
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.business/domains/business"
+)
+
+type BusinessGetWithUserQuery struct {
+	NickName string
+	UserName string
+	UserUUID string
+}
+
+type BusinessGetWithUserRes struct {
+	Dto *business.EntityWithUserDto
+}
+
+type BusinessGetWithUserHandler cqrs.HandlerFunc[BusinessGetWithUserQuery, *BusinessGetWithUserRes]
+
+func NewBusinessGetWithUserHandler(repo business.Repository) BusinessGetWithUserHandler {
+	return func(ctx context.Context, query BusinessGetWithUserQuery) (*BusinessGetWithUserRes, *i18np.Error) {
+		business, err := repo.GetWithUser(ctx, query.NickName, business.UserDetail{
+			Name: query.UserName,
+			UUID: query.UserUUID,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return &BusinessGetWithUserRes{Dto: business}, nil
+	}
+}
