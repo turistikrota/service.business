0b376d216bcc45e4a03a097d454c1b23f4757263
9ssi7 <info@ssibrahimbas.com>
2024-03-26 01:30:34 +0300

    feature/cillop: add invite queries

--- app/query/invite_get_by_business_uuid.go
+++ app/query/invite_get_by_business_uuid.go
@@ -1,1 +1,29 @@
 package query
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.business/domains/invite"
+)
+
+type InviteGetByBusinessUUIDQuery struct {
+	BusinessUUID string
+}
+
+type InviteGetByBusinessUUIDRes struct {
+	Invites []*invite.Entity
+}
+
+type InviteGetByBusinessUUIDHandler cqrs.HandlerFunc[InviteGetByBusinessUUIDQuery, *InviteGetByBusinessUUIDRes]
+
+func NewInviteGetByBusinessUUIDHandler(repo invite.Repository, factory invite.Factory) InviteGetByBusinessUUIDHandler {
+	return func(ctx context.Context, query InviteGetByBusinessUUIDQuery) (*InviteGetByBusinessUUIDRes, *i18np.Error) {
+		invites, err := repo.GetByBusinessUUID(ctx, query.BusinessUUID)
+		if err != nil {
+			return nil, factory.Errors.Failed(err.Error())
+		}
+		return &InviteGetByBusinessUUIDRes{Invites: invites}, nil
+	}
+}
--- app/query/invite_get_by_uuid.go
+++ app/query/invite_get_by_uuid.go
@@ -1,1 +1,29 @@
 package query
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.business/domains/invite"
+)
+
+type InviteGetByUUIDQuery struct {
+	UUID string `params:"uuid" validate:"required,object_id"`
+}
+
+type InviteGetByUUIDRes struct {
+	Invite *invite.Entity
+}
+
+type InviteGetByUUIDHandler cqrs.HandlerFunc[InviteGetByUUIDQuery, *InviteGetByUUIDRes]
+
+func NewInviteGetByUUIDHandler(repo invite.Repository, factory invite.Factory) InviteGetByUUIDHandler {
+	return func(ctx context.Context, query InviteGetByUUIDQuery) (*InviteGetByUUIDRes, *i18np.Error) {
+		invite, err := repo.GetByUUID(ctx, query.UUID)
+		if err != nil {
+			return nil, factory.Errors.Failed(err.Error())
+		}
+		return &InviteGetByUUIDRes{Invite: invite}, nil
+	}
+}
