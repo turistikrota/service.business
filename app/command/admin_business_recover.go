476d462c5749baa3cfba7e8d5771565d6639cffd
9ssi7 <info@ssibrahimbas.com>
2024-03-25 23:14:07 +0300

    feature/cillop: add the recover cmd

--- app/command/admin_business_recover.go
+++ app/command/admin_business_recover.go
@@ -1,1 +1,32 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.business/domains/business"
+)
+
+type AdminBusinessRecoverCmd struct {
+	BusinessName string
+	AdminUUID    string
+}
+
+type AdminBusinessRecoverRes struct{}
+
+type AdminBusinessRecoverHandler cqrs.HandlerFunc[AdminBusinessRecoverCmd, *AdminBusinessRecoverRes]
+
+func NewAdminBusinessRecoverHandler(repo business.Repository, factory business.Factory, events business.Events) AdminBusinessRecoverHandler {
+	return func(ctx context.Context, cmd AdminBusinessRecoverCmd) (*AdminBusinessRecoverRes, *i18np.Error) {
+		err := repo.Recover(ctx, cmd.BusinessName)
+		if err != nil {
+			return nil, err
+		}
+		events.RecoverByAdmin(&business.EventBusinessRecoverByAdmin{
+			AdminUUID:    cmd.AdminUUID,
+			BusinessName: cmd.BusinessName,
+		})
+		return &AdminBusinessRecoverRes{}, nil
+	}
+}
