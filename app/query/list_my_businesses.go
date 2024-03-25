696c2357b776439c01428168e4a79e24784c3481
9ssi7 <info@ssibrahimbas.com>
2024-03-26 01:37:02 +0300

    feature/cillop: add the all queries

--- app/query/list_my_businesses.go
+++ app/query/list_my_businesses.go
@@ -1,1 +1,33 @@
 package query
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.business/domains/business"
+)
+
+type ListMyBusinessesQuery struct {
+	UserName string
+	UserUUID string
+}
+
+type ListMyBusinessesRes struct {
+	Businesses []*business.Entity
+}
+
+type ListMyBusinessesHandler cqrs.HandlerFunc[ListMyBusinessesQuery, *ListMyBusinessesRes]
+
+func NewListMyBusinessesHandler(repo business.Repository, factory business.Factory) ListMyBusinessesHandler {
+	return func(ctx context.Context, query ListMyBusinessesQuery) (*ListMyBusinessesRes, *i18np.Error) {
+		businesss, err := repo.ListByUserUUID(ctx, business.UserDetail{
+			Name: query.UserName,
+			UUID: query.UserUUID,
+		})
+		if err != nil {
+			return nil, factory.Errors.Failed(err.Error())
+		}
+		return &ListMyBusinessesRes{Businesses: businesss}, nil
+	}
+}
--- app/query/view_business.go
+++ app/query/view_business.go
@@ -1,1 +1,29 @@
 package query
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.business/domains/business"
+)
+
+type ViewBusinessQuery struct {
+	NickName string `params:"nickName" validate:"required"`
+}
+
+type ViewBusinessRes struct {
+	Business *business.Entity
+}
+
+type ViewBusinessHandler cqrs.HandlerFunc[ViewBusinessQuery, *ViewBusinessRes]
+
+func NewViewBusinessHandler(repo business.Repository) ViewBusinessHandler {
+	return func(ctx context.Context, query ViewBusinessQuery) (*ViewBusinessRes, *i18np.Error) {
+		b, err := repo.ProfileView(ctx, query.NickName)
+		if err != nil {
+			return nil, err
+		}
+		return &ViewBusinessRes{Business: b}, nil
+	}
+}
