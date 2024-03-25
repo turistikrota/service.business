baf38c7b4116df1b5cb4b469dd5e027f3f4f96b5
9ssi7 <info@ssibrahimbas.com>
2024-03-25 23:15:23 +0300

    feature/cillop: add reject cmd

--- app/command/admin_business_reject.go
+++ app/command/admin_business_reject.go
@@ -1,1 +1,33 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.business/domains/business"
+)
+
+type AdminBusinessRejectCmd struct {
+	Reason       string `json:"reason" validate:"required,min=1,max=500"`
+	BusinessName string
+	AdminUUID    string
+}
+
+type AdminBusinessRejectRes struct{}
+
+type AdminBusinessRejectHandler cqrs.HandlerFunc[AdminBusinessRejectCmd, *AdminBusinessRejectRes]
+
+func NewAdminBusinessRejectHandler(repo business.Repository, factory business.Factory, events business.Events) AdminBusinessRejectHandler {
+	return func(ctx context.Context, cmd AdminBusinessRejectCmd) (*AdminBusinessRejectRes, *i18np.Error) {
+		err := repo.Reject(ctx, cmd.BusinessName, cmd.Reason)
+		if err != nil {
+			return nil, err
+		}
+		events.RejectedByAdmin(&business.EventBusinessRejectedByAdmin{
+			AdminUUID:    cmd.AdminUUID,
+			BusinessName: cmd.BusinessName,
+		})
+		return &AdminBusinessRejectRes{}, nil
+	}
+}
